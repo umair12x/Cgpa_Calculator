f9dd4d0207486dff0055f76a64b96d63332c4518
mian95 <umairim24@gmail.com>
2026-01-12 23:38:08 +0500

    apikey_middleware

--- server/app.js
+++ server/app.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 const port = process.env.PORT || 5000;
+const apiKeyMiddleware = require("./middlewares/api_key.js");
 const router = require("./routes/result-route.js");
 
-
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173", "https://yourdomain.vercel.app"],
   methods: "GET,POST,PUT,PATCH,HEAD",
-  credentials: true, 
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(apiKeyMiddleware);
 
 app.use("/Cgpa", router);
 
